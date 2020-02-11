1781b64d1d95872ba2320f52d2e0645c0c75f254
ajshemi <akpojotor@yahoo.com>
2020-02-10 21:42:45 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+const arrayOfMovies=movies.map(movie=>(<div>{`${movie.title} ${movie.time}`} {movie.genres.map(genre=><ul>{`${genre}`}</ul>)}</div> ))
+// const arrayofGenres=movies.map(movie=>movie.genres.map(genre=>(<ul>{`${genre}`}</ul>)))
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        
+          {arrayOfMovies}
+          {/* {arrayofGenres} */}
+  
     </div>
   );
 };
